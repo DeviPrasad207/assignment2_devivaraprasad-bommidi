8026b13d9ee8b3091f5e8f7006d3404493ac8d58
DeviPrasad207 <56407988+DeviPrasad207@users.noreply.github.com>
2019-11-03 18:21:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+/*
+<!—
+===========================================
+Student Name: DEVI VARA PRASAD BOMMIDI
+Solution for Info 600 - Assignment 2
+===========================================
+-->
+*/
+//Javascript code to create user interface for Student Record Management/Entry
+function myRecord(){
+	 var time = new Date().getHours();
+	 var minutes = new Date().getMinutes();
+	 var seconds = new Date().getSeconds();
+	 var studentname= document.getElementById("firstname").value+ "";
+	 var studentmajor= document.getElementById("major").value+ "";
+	 var studentyear= document.getElementById("startyear").value+ "";
+//Conditions to validate the year null fields
+	 if (studentyear <= 2000)
+		  {
+		    alert ("Incorrect Year, please enter a year greater than or equal to 2000");
+			}
+	  if (studentname == " ")
+                {
+                                alert ("Please enter name");
+                }
+      if(studentmajor == " ")
+                {
+                                alert ("Please enter major");
+                }
+
+	if (studentyear >= 2000 && studentname != " " && studentmajor != " ")
+	{
+//To display entered records to the user interface	    
+	 document.getElementById("records").innerHTML += "<br />" +time+":"+minutes+":"+seconds+", "+studentname+", "+studentmajor+", "+studentyear;
+	 document.MyFormDetails.firstname.value=" ";
+	 document.MyFormDetails.major.value=" ";
+	 document.MyFormDetails.startyear.value=" "; 
+	 }
+
+	 }
